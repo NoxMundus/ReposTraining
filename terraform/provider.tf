7507fb75bd39335e95a32c04e109aad2c3857dba
Ricardo Rossi <ricardozrossi@outlook.com>
2024-06-12 11:22:12 -0300

    banana2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = var.region
-#    access_key = "AKIA5BFBJIZ24CQXVDOX"
-#    secret_key = "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"
+#    access_key = ""
+#    secret_key = ""
 }
